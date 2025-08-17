7f7d2fd70d591b094965cb8682f6fa4116730b66
Ahnaf Tahmid Chowdhury <tahmid@nukehub.org>
2025-08-17 12:06:49 +0600

    add sw

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+/*TODO  Service Worker */
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
+
 /*TODO  Console Warning */
 console.log(
   "%cStop!",
